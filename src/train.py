d4a9265bfaecfe5c3965c74878a621b2d553b27f
Shayne O'Brien <shayneobrien@users.noreply.github.com>
2018-06-05 23:26:38 -0400

    TODO: fix backprop bug

--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-from modules import *
+from modules import TextSeg, Trainer
 
 model = TextSeg(lstm_dim=300, score_dim=300, bidir=True, num_layers=2)
 trainer = Trainer(model, '../data/wiki_727/train', batch_size=20)
