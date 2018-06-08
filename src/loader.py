a58150b753c8cdf66d26e5e9dd2aad075bd36502
Shayne O'Brien <shayneobrien@users.noreply.github.com>
2018-06-08 13:22:57 -0400

    added functionality to Batch class

--- src/loader.py
+++ src/loader.py
@@ -11,33 +11,46 @@ class Batch:
     """ Contains many documents for efficient computation """
     def __init__(self, documents):
         self.documents = documents
+        self.ix, self.sents, self.labels = self.unravel(documents)
     
     def __getitem__(self, idx):
-        return self.documents[idx]
+        return self.sents[idx]
 
     def __repr__(self):
-        return 'Batch containing %d documents' % (len(self.documents))
+        return ('Batch of %d sentences from %d documents' 
+                % (len(self.sents), len(self.documents)))
     
     def __len__(self):
-        return len(self.documents)
+        return len(self.sents)
+    
+    def unravel(self, documents):
+        lengths = [0] + [len(d) for d in documents]
+        ix = [sum(lengths[:idx+1]) for idx, _ in enumerate(lengths)]
+        sents = flatten([d.sents for d in documents])
+        labels = torch.cat([d.labels for d in documents])
+        return ix, sents, labels
+        
+    def regroup(self, groups):
+        regrouped = [groups[self.ix[i]:self.ix[i+1]] for i in range(len(self.ix)-1)]
+        return regrouped
 
 
 class Document:
     """ Contains all sentences in a Wiki article, whether they end a 
     subsection, and the document's filename """
     def __init__(self, sentences, labels, filename):
-        self.sentences = sentences
-        self.labels = labels
+        self.sents = sentences
+        self.labels = torch.tensor(labels)
         self.filename = filename
     
     def __getitem__(self, idx):
-        return self.sentences[idx]
+        return self.sents[idx]
 
     def __repr__(self):
-        return 'Document containing %d sentences' % (len(self.sentences))
+        return 'Document containing %d sentences' % (len(self.sents))
     
     def __len__(self):
-        return len(self.sentences)
+        return len(self.sents)
 
 
 class Sentence:
@@ -62,7 +75,7 @@ class LazyVectors:
     
     unk_idx = 1
 
-    def __init__(self, name='glove.840B.300d.txt'):
+    def __init__(self, name='glove.840B.300d.txt', skim=None):
         """ Requires the glove vectors to be in a folder named .vector_cache
         
         In Bash/Terminal from directory this class is in:
@@ -70,6 +83,7 @@ class LazyVectors:
         >> mv glove.840B.300d.txt .vector_cache/glove.840B.300d.txt
         """
         self.name = name
+        self.skim = skim # For debugging; loads faster.
         self.set_vocab()
 
     @cached_property
@@ -77,6 +91,7 @@ class LazyVectors:
         return Vectors(self.name)
     
     def get_vocab(self, filename='vocabulary.txt'):
+        """ Read in vocabulary (top 30K words, covers ~93.5% of all tokens) """ 
         with open(filename, 'r') as f:
             vocab = f.read().split(',')
         return vocab
@@ -84,7 +99,8 @@ class LazyVectors:
     def set_vocab(self):
         """Set corpus vocab """
         # Intersect with model vocab.
-        self.vocab = [v for v in self.get_vocab() if v in self.loader.stoi]
+        self.vocab = [v for v in self.get_vocab()
+                      if v in self.loader.stoi][:self.skim]
 
         # Map string -> intersected index.
         self._stoi = {s: i for i, s in enumerate(self.vocab)}
@@ -104,8 +120,7 @@ class LazyVectors:
         ])
 
     def stoi(self, s):
-        """Map string -> embedding index.
-        """
+        """ String to index (s to i) for embedding lookup """
         idx = self._stoi.get(s)
         return idx + 2 if idx else self.unk_idx
 
@@ -118,6 +133,12 @@ def crawl_directory(dirname):
             if not name.endswith('.DS_Store'):
                 yield os.path.join(path, name)
                 
+def sample_and_batch(*args):
+    """ Sample some directory path and Batch the documents """
+    files = sample_nested_dir(*args)
+    documents = [read_document(f) for f in files]
+    return Batch(documents)
+                
 def sample_and_read(*args):
     """ Sample some directory path for batch_size number of documents """
     files = sample_nested_dir(*args)
@@ -125,7 +146,6 @@ def sample_and_read(*args):
         yield read_document(f)
                 
 def sample_nested_dir(directory, batch_size=100):
-    # TODO: make recursive
     """ Sample files from a nested directory """
     samples, dirname = [], directory
     while len(samples) < batch_size:
@@ -143,21 +163,34 @@ def safe_listdir(dirname):
     """ Listdir but without .DS_Store hidden file """
     return [f for f in os.listdir(dirname) if not f.endswith('.DS_Store')]
 
-def read_document(filename, minlen=0):
-    """ Read in a Wiki-727 file """
+def read_document(filename, minlen=1):
+    """ Read in a Wiki-727 file. 
+    Only keep documents longer than minlen subsections. """
+    
+    # Initialize, open file
     document, subsection = [], ''
     with open(filename) as f:
+        
+        # For each line in the file
         for line in f.readlines()[1:]:
+            
+            # This '========' indicates a new subsection
             if line.startswith('========'):
                 document.append(sent_tokenizer.tokenize(subsection.strip()))
                 subsection = ''
             else:
-                subsection += ' ' + line[:-1] # Exclude \n 
-        document.append(sent_tokenizer.tokenize(subsection.strip()))
+                subsection += ' ' + line[:-1] # Exclude '\n' from the line
         
-    document = [subsection for subsection in document if len(subsection) > minlen]
+        # Edge case of last subsection needs to be appended
+        document.append(sent_tokenizer.tokenize(subsection.strip()))
+    
+    # Keep only subsections longer than minlen
+    document = [subsection for subsection in document if len(subsection) >= minlen]
+    
+    # Compute labels for the subsections
     labels = doc_to_labels(document)
 
+    # Organize the data into objects, defined above.
     document = Document([Sentence(text, label) for text, label in zip(flatten(document), labels)], 
                          labels,
                          filename)
@@ -170,8 +203,8 @@ def doc_to_labels(document):
     return flatten([(len(doc)-1)*[0] + [1] for doc in document])
 
 def clean_token(token):
-    """  Remove everything but whitespace, the alphabet; 
-    separate apostrophes for stopwords """
+    """ Remove everything but whitespace, the alphabet, digits; 
+    separate apostrophes for stopwords; replace only digit tokens """
     if token.isdigit():
         token = '#NUM'
     else:
