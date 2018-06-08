a0e0c37c66a4ee37e11c9339e52e6138f120484f
Shayne O'Brien <shayneobrien@users.noreply.github.com>
2018-06-08 13:25:30 -0400

    slight format change

--- src/train.py
+++ src/train.py
@@ -1,5 +1,11 @@
 from modules import TextSeg, Trainer
 
-model = TextSeg(lstm_dim=300, score_dim=300, bidir=True, num_layers=2)
-trainer = Trainer(model, '../data/wiki_727/train', batch_size=20)
-trainer.train(30)
+model = TextSeg(lstm_dim=200, score_dim=300, bidir=True, num_layers=2)
+
+trainer = Trainer(model=model,
+                  train_dir='../data/wiki_727/train', 
+                  val_dir='../data/wiki_50',
+                  batch_size=10,
+                  lr=1e-3)
+
+trainer.train(num_epochs=30, steps=10)
